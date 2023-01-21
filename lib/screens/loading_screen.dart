12a4c429bfb5934f17263a6ff61cd0e187f838cd
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2023-01-21 16:14:02 +1100

    Use initstate

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -48,20 +48,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   }
 
-
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            getLocation();
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
+
     );
   }
 }
